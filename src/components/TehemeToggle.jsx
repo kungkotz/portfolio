c43dd6222ccb3073d90a5dc8df6d5bb371ba34b8
kotz <kotzspam@gmail.com>
2025-06-25 14:03:11 +0200

    added localStorage for dark/light mode

--- src/components/TehemeToggle.jsx
+++ src/components/TehemeToggle.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Sun, Moon } from "lucide-react";
 
 export const TehemeToggle = () => {
 	const [isDarkMode, setIsDarkMode] = useState(false);
 
+	useEffect(() => {
+		const storedTheme = localStorage.getItem("theme");
+		if (storedTheme === "dark") {
+			setIsDarkMode(true);
+			document.documentElement.classList.add("dark");
+		} else {
+			localStorage.setItem("theme", "light");
+			setIsDarkMode(false);
+		}
+	}, []);
 	const toggleTheme = () => {
 		if (isDarkMode) {
 			document.documentElement.classList.remove("dark");
+			localStorage.setItem("theme", "light");
 			setIsDarkMode(false);
 		} else {
 			document.documentElement.classList.add("dark");
+			localStorage.setItem("theme", "dark");
 			setIsDarkMode(true);
 		}
 	};
